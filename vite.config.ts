90d8e6c536f0c6c38861044121a9d6c7ceab3c88
Mohit Kumar <110058039+Mohitkr456@users.noreply.github.com>
2025-09-05 01:25:00 +0530

    Fix imagetools processing error

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-import { imagetools } from "vite-imagetools";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -25,7 +24,6 @@ export default defineConfig(({ mode }) => ({
       avif: { quality: 50 },
       svg: { multipass: true },
     }),
-    imagetools(),
   ].filter(Boolean),
   resolve: {
     alias: {
